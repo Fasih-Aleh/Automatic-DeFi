1d142fc2bd8cd2f2d59f22f225338d9a69970be4
Fasih-Aleh <fasih@aleh.tech>
2023-06-02 21:26:41 +0500

    :construction: Rebating

---
+++ contracts/custom/Rebate.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+
+contract Rebate {
+    constructor() public {
+    }
+
+    struct record {
+        uint256 amount;
+        uint256 timestamp;
+        uint256 price;
+    }
+
+    
+
+}
