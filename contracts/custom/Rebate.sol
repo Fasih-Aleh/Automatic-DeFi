14f2f6b174c1d4f26272747f34d7496dc0337f0e
Fasih-Aleh <fasih@aleh.tech>
2023-06-05 16:04:01 +0500

    :construction: Rebating: add/remove liquidity

--- contracts/custom/Rebate.sol
+++ contracts/custom/Rebate.sol
@@ -11,16 +11,36 @@ contract Rebate {
         uint256 timestamp;
     }
 
-    mapping(address => mapping (uint => record)) records;
+    mapping(address => mapping (uint256 => record)) records;
+
+    mapping(address => uint256) liquidityAmount;
 
     mapping(address => uint256) liquidityCount;
 
     mapping(address => uint256) liquidityIndex;
 
     function addLiquidity(address _account, uint256 _usdgAmount) external {
+        uint256 _liquidityCount = liquidityCount[_account];
+        records[_account][_liquidityCount] = record(_usdgAmount, block.timestamp);
+        liquidityCount[_account] += 1;
+        liquidityAmount[_account] += _usdgAmount;
     }
 
     function removeLiquidty(address _account, uint256 _usdgAmount) external {
-        
+        require(_usdgAmount <= liquidityAmount[_account], "not enough liquidity");
+        uint256 removedLiquidity = 0;
+        uint256 _liquidityIndex = liquidityIndex[_account];
+        while(removedLiquidity < _usdgAmount) {
+            if (_usdgAmount - removedLiquidity < records[_account][liquidityIndex].usdgAmount) {
+                uint256 liquidityToRemove = _usdgAmount - removedLiquidity;
+                records[_account][liquidityIndex].usdgAmount -= liquidityToRemove;
+                removedLiquidity += liquidityToRemove;
+            }
+            else {
+                uint256 liquidityToRemove = records[_account][liquidityIndex];
+                records[_account][liquidityIndex].usdgAmount = 0;
+                liquidityIndex[_account] += 1;
+            }
+        }        
     }
 }
